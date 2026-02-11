00bee4b9b1f99012a9568817aa23bde611325b94
px7nn <px7nnn@gmail.com>
2026-02-12 05:24:01 +0530

    almost. . .

--- scripts/text.js
+++ scripts/text.js
@@ -1,3 +1,60 @@
-const text = document.querySelector('c-text').innerHTML
+const c_text = document.querySelector('.c-text')
+const yesBTN = document.querySelector('.yes')
+const noBTN = document.querySelector('.no')
+const text = [
+    "I miss you/"+ 
+    "And I hate the/"+ 
+    "distance between us/",
+
+    "Every second without you/"+ 
+    "feels longer than it should/",
+
+    "So tell me…/",
+
+    "Will you be mine?/"+
+    "/"+
+    "Will you be my valentine...??"
+]
+
+let indx = 0, isTyping = false;
+ShowText()
+
+yesBTN.addEventListener("click", () => {
+    if(!isTyping) YesHandler();
+})
+
+function YesHandler(){
+    if(indx >= text.length) return
+    if(indx === text.length - 1){
+        yesBTN.innerHTML = "Yes"
+    }
+
+    ShowText()
+
+}
+function ShowText(){
+    c_text.innerHTML = "";
+    isTyping = true;
+    yesBTN.classList.add('hide'); 
+    let delay = 50
+    for(let ch of text[indx]){
+        setTimeout(()=>{
+            if(ch==="/")
+                c_text.innerHTML += "<br>"
+            else
+                c_text.innerHTML += ch
+        }, delay)
+        delay += 50 + Math.random() * (100)
+    }
+    setTimeout(()=>{
+        yesBTN.classList.remove('hide');
+        if(indx === text.length - 1)
+            noBTN.classList.remove('hide')
+        indx++;
+        isTyping = false;
+    }, delay)
+
+}
+
 
 console.log(text);
--- scripts/yes_no.js
+++ scripts/yes_no.js
@@ -1,16 +1,18 @@
 const no = document.querySelector('.no')
-        const yes = document.querySelector('.yes')
+const yes = document.querySelector('.yes')
 
-        no.addEventListener('mouseenter', () => {
-            const x = Math.random() * window.innerWidth * 0.6 - 100;
-            const y = Math.random() * window.innerHeight * 0.6 - 50;
+no.addEventListener('mouseenter', () => {
+    const x = Math.random() * window.innerWidth * 0.6 - 100;
+    const y = Math.random() * window.innerHeight * 0.6 - 50;
 
-            no.style.position = "absolute";
-            no.style.left = `${x}px`;
-            no.style.top = `${y}px`;
-        })
+    no.style.position = "absolute";
+    no.style.left = `${x}px`;
+    no.style.top = `${y}px`;
+})
 
-        yes.addEventListener('mouseup', () =>{
-            document.querySelectorAll('.opt').forEach(e => e.classList.add('hide'));
-            document.querySelector('.cat').classList.add('cat-animate');
-        })
+yes.addEventListener('mouseup', () =>{
+    if(yes.innerHTML == "Yes"){
+        document.querySelectorAll('.opt').forEach(e => e.classList.add('hide'));
+        document.querySelector('.cat').classList.add('cat-animate');
+    }
+})
